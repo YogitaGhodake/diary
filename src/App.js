def26a2b227b438d344497402d3077656341b41f
Yogita Ghodake <yogitaghodake93@gmail.com>
2023-01-06 17:01:16 +0530

    css changes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import MyBook from './components/HTMLFlipBook/myBook';
 import 'font-awesome/css/font-awesome.min.css';
 
+
 function App() {
   return (
     <div className="App">
       <MyBook />
+    
     </div>
   );
 }
--- src/components/HTMLFlipBook/myBook.js
+++ src/components/HTMLFlipBook/myBook.js
@@ -1,73 +1,127 @@
 import React from "react";
 // import { Link } from "react-router-dom";
 import HTMLFlipBook from "react-pageflip";
-import './myBook.css';
-import '../../../src/index.css';
-import 'font-awesome/css/font-awesome.min.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import "../PlannerBook/PlannerBook.scss";
+import "../../../src/index.css";
+import  './myBook.css';
+import "font-awesome/css/font-awesome.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleQuestion } from "@fortawesome/free-regular-svg-icons";
+import AboutBook from "../About/aboutBook";
 
-// const PageCover = React.forwardRef((props, ref) => {
-//   return (
-//     <div className="page page-cover" ref={ref} data-density="hard">
-//       <div className="page-content">
-//         <h2>{props.children}</h2>
-//       </div>
-//     </div>
-//   );
-// });
-
-// const Page = React.forwardRef((props, ref) => {
-//   return (
-//     <div className="page" ref={ref}>
-//       <div className="page-content">
-//         <h2 className="page-header">Page header - {props.number}</h2>
-//         <div className="page-image"></div>
-//         <div className="page-text">{props.children}</div>
-//         <div className="page-footer">{props.number + 1}</div>
-//       </div>
-//     </div>
-//   );
-// });
 
 
 
 export default class MyBook extends React.Component {
   constructor(props) {
-    super(props);
+    super(props);                                                
 
     this.state = {
       page: 0,
       totalPage: 0,
       // height: props.height,
       bookPages: [
-       
-        // "Separator.jpg",
-        "CoversFront.jpg",
-        // "Separator.jpg",
-        "page_1.jpg",
-        "page_2.jpg",
-        "page_3.jpg",
-        "page_4.jpg",
-        "page_5.jpg",
-        "page_6.jpg",
-        "page_7.jpg",
-        "page_8.jpg",
-        "page_9.jpg",
-        "page_10.jpg",
-        "page_11.jpg",
-        "page_12.jpg",
-        "page_13.jpg",
-        "page_14.jpg",
-        "page_15.jpg",
-        "page_16.jpg",
-        "CoversBack.jpg",
-      ],
-     
+      {
+        pageImg: "CoversFront.jpg",
+        showTriggerBtn: false
+      },
+      {
+        pageImg:"page_6.jpg",
+        showTriggerBtn: false
+      },
+      {
+        pageImg: "page_1.jpg",
+        showTriggerBtn: true
+      },
+      {
+        pageImg: "page_2.jpg",
+        showTriggerBtn: true
+      },
+      {
+        pageImg: "page_3.jpg",
+        showTriggerBtn: true
+      },
+      {
+        pageImg: "page_4.jpg",
+        showTriggerBtn: true
+      },
+      {
+        pageImg:"page_5.jpg",
+        showTriggerBtn: true
+      },
+      {
+        pageImg: "page_6.jpg",
+        showTriggerBtn: false
+      },
+      {
+        pageImg: "page_7.jpg",
+        showTriggerBtn: true
+      },
+      {
+        pageImg:  "page_8.jpg",
+        showTriggerBtn: true
+      },
+      {
+        pageImg:  "page_10.jpg",
+        showTriggerBtn: true
+      },
+      {
+        pageImg:"page_11.jpg",
+        showTriggerBtn: true
+      },
+      {
+        pageImg: "page_12.jpg",
+        showTriggerBtn: true
+      },
+      {
+        pageImg: "page_13.jpg",
+        showTriggerBtn: true
+      },
+      {
+        pageImg: "page_14.jpg",
+        showTriggerBtn: true
+      },
+      {
+        pageImg: "page_15.jpg",
+        showTriggerBtn: true
+      },
+      {
+        pageImg: "page_16.jpg",
+        showTriggerBtn: true
+      },
+      {
+        pageImg: "CoversBack.jpg",
+        showTriggerBtn: false
+      },
+      
+    ]
+
+
+      // bookPages: [
+      //   "CoversFront.jpg",
+      //   "page_6.jpg",
+      //   "page_1.jpg",
+      //   "page_2.jpg",
+      //   "page_3.jpg",
+      //   "page_4.jpg",
+      //   "page_5.jpg",
+      //   "page_6.jpg",
+      //   "page_7.jpg",
+      //   "page_8.jpg",
+      //   "page_9.jpg",
+      //   "page_10.jpg",
+      //   "page_11.jpg",
+      //   "page_12.jpg",
+      //   "page_13.jpg",
+      //   "page_14.jpg",
+      //   "page_15.jpg",
+      //   "page_16.jpg",
+      //   "page_6.jpg",
+      //   "CoversBack.jpg",
+      // ],
     };
   }
 
- 
   nextButtonClick = () => {
     this.flipBook.getPageFlip().flipNext();
   };
@@ -88,57 +142,69 @@ export default class MyBook extends React.Component {
     });
   }
 
-  componentWillMount(){
-    this.setState({height: window.innerHeight + 'px'});
+  componentWillMount() {
+    this.setState({ height: window.innerHeight + "px" });
   }
 
-  
   render() {
     return (
-      <div className="main demo-block bg-lightx pt-3 pb-3 overflow-hidden" id="demoBlock">
-
+      <div
+        className="main demo-block bg-lightx pt-3 pb-3 overflow-hidden"
+        id="demoBlock"
+      >
         <div className="">
-
-        <div className="container-md" style={{position:"relative"}}>
-          <div className="flip-book html-book demo-book stf__parent" style={{ minWidth: '315px', minHeight: '400px', width: '100%', maxWidth: '2000px', display: 'block'}}>
-          <div className="stf__wrapper --landscape">
-          <div classN ame="stf__block">
-          <HTMLFlipBook
-          width={600}
-          height={790}
-          showCover={true}
-          size= {"stretch"}
-          ref={(el) => (this.flipBook = el)}
-        >
-         
-         { this.state.bookPages.map((page, index) => {
-          return(
-            <div className="demoPage page page-cover page-cover-top stf__item --right --hard">
-              <div className="tag">
-             <a style={{textDecoration: "none", color:"red"}} href="https://www.youtube.com/" > <FontAwesomeIcon icon={faCircleQuestion} />
-            </a>
-                
+          <div className="container-md" style={{ position: "relative" }}>
+            <div
+              className="flip-book html-book demo-book stf__parent" 
+            >
+              <div className="stf__wrapper --portrait" >
+                <div classN ame="stf__block">
+                  <HTMLFlipBook
+                    width={500}
+                    height={650}
+                    showCover={true}
+                    ref={(el) => (this.flipBook = el)}
+                  >
+                    {this.state.bookPages.map((page, index) => {
+                      console.log("index coverback", this.state.bookPages.length);
+                      return (
+                        <div className="pageDiv demoPage page page-cover page-cover-top stf__item --right --hard">
+                          { page.showTriggerBtn === false ? ( null
+                          ) : (
+                            <div className="tag">
+                              <a
+                                style={{ textDecoration: "none", color: "red" }}
+                                href='https://www.youtube.com/'
+                                
+                              >
+                                <FontAwesomeIcon icon={faCircleQuestion} />
+                              </a>
+                              {/* <Link to={<AboutBook/>}></Link> */}
+                            </div>
+                          )}
+                          <img
+                            // src={"/assets/images/"+ page}
+                            src={
+                              "https://thumbstack.in/wp-content/uploads/2023/01/" +
+                              page.pageImg
+                            }
+                            className="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}"
+                            alt=""
+                            width={320}
+                            // minWidth={350} minHeight={500}
+                            height={400}
+                          />
+                          {/* Page {index + 1} */}
+                        </div>                                                                                                                                                                                         
+                      );
+                    })}
+                  </HTMLFlipBook>
+                  {/* <AboutBook /> */}
+                 </div>
               </div>
-              <img
-              // src={"/assets/images/"+ page}
-              src={"../images/" + page}
-              className="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}"
-              alt="" width={550} height='auto'
-            /> 
-            Page {index+1}
-          </div>
-          )
-        }) }
-            </HTMLFlipBook>
             </div>
-          
           </div>
-          </div>
-        
-        </div>
-        
-
-        {/* <div className="container">
+          {/* <div className="container">
           <div>
             <button type="button" onClick={this.prevButtonClick}>
               Previous page
@@ -150,7 +216,7 @@ export default class MyBook extends React.Component {
             </button>
           </div>
         </div> */}
-      </div>
+        </div>
       </div>
     );
   }
